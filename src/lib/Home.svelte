1b953b4cef9449ec734fed8bde9a62b15f821b10
Adi <adi92az@gmail.com>
2023-10-28 19:17:31 +0200

    Add hoe component

---
+++ src/lib/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ModuleButton from "./ModuleButton.svelte";
+
+</script>
+
+<div class="module-btn-container" >
+    <ModuleButton moduleName="Hiragana Chart" />
+    <ModuleButton moduleName="Words" />
+</div>
+
+<style>
+    .module-btn-container {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        justify-content: center;
+        height: 100%;
+    }
+</style>
