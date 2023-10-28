5d0c706b664342ec9aee550bb00ffd2529bfc5e6
Adi <adi92az@gmail.com>
2023-10-28 19:16:10 +0200

    Add words components

---
+++ src/lib/Word.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Hiragana from "./Hiragana.svelte";
+
+   export let word 
+   export let mode = "hiragana"
+
+   $: romajiWord = word ? word.romaji.split(" ") : ""
+</script>
+
+{#if word}
+    <div class="word-container" >
+        {#if mode === "hiragana"}
+            {#each romajiWord  as romaji}
+                <Hiragana letter={romaji} />
+            {/each}
+        {:else if mode === "english"}
+            <p>{word.english}</p>
+        {:else}
+            <p>{word.romaji}</p>
+        {/if}
+    </div>
+{/if}
+
+<style>
+    .word-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    p {
+        color: black;
+    }
+</style>
---
+++ src/lib/Words.svelte
@@ -0,0 +1,97 @@
+<script>
+    import { WordsStore } from "../stores";
+
+    import Word from "./Word.svelte";
+
+    let index = 0
+
+    let mode = "hiragana"
+
+    function previous() {
+       if(index > 0) {
+        index -= 1
+        resetMode()
+       }
+    }
+
+    function next() {
+        if(index < $WordsStore.length - 1) {
+            index += 1
+            resetMode()
+        }
+    }
+
+    function changeMode() {
+        mode = "english"
+    }
+
+    function resetMode() {
+        mode = "hiragana"
+    }
+
+    $: currentWord = $WordsStore[index]
+</script>
+
+<div class="words" >
+    {#if $WordsStore.length > 0}
+        <div class="word-container" >
+            <div class="word-card" on:click={changeMode} >
+                <Word word="{currentWord}" {mode} />
+            </div>
+        </div>
+    {/if}
+    
+    <div class="slide" >
+        <div on:click={previous} class="left" ></div>
+        <div on:click={next} class="right" ></div>
+    </div>
+</div>
+
+
+<style>
+    .words {
+        height: 100%;
+        position: relative;
+    }
+
+    .word-container {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .word-card {
+        background-color: white;
+        position: relative;
+        z-index: 10;
+        border-radius: 1rem;
+        height: 50vw;
+        width: 250px;
+        padding: 25px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .slide {
+        position: absolute;
+        display: flex;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+       
+    }
+
+    .left {
+        height: 90%;
+        width: 50%;
+    }
+
+    .right {
+        height: 90%;
+        width: 50%;
+    }
+
+</style>
