23c1bf672ee4a7ed0beb12dd4f591ae8f213fcad
Adi <adi92az@gmail.com>
2023-10-28 19:16:48 +0200

    Add module button component

---
+++ src/lib/ModuleButton.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { ModuleStore } from "../stores";
+
+    export let moduleName
+
+    function setModule() {
+        ModuleStore.set(moduleName)
+    }
+</script>
+
+<button on:click={setModule} class="module-button" >
+    {moduleName}
+</button>
+
+<style>
+    .module-button {
+        border: 1px solid white;
+        padding: 20px 40px;
+    }
+</style>
