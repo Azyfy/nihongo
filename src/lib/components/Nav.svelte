20eb28c5b2d7e767528134124828f0f17f74bfc3
Adi <adi92az@gmail.com>
2023-11-30 15:13:52 +0100

    Replace nav btn png with svg

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,10 +1,8 @@
 <script>
     import { ModuleStore } from "../../stores";
-    import { IconHome } from "../icons";
-    import Kana from "./Kana.svelte";
+    import { IconHome, IconKana } from "../icons";
 
     function setModule(module) {
-        console.log("MOD", module)
         ModuleStore.set(module)
     }
 </script>
@@ -13,8 +11,8 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="nav" >
     {#if $ModuleStore === "Kana Chart" || $ModuleStore === "Hiragana" || $ModuleStore === "Katakana"}
-        <div on:click={() => {setModule('Hiragana')}} class="nav-button img-button {$ModuleStore === "Kana Chart" || $ModuleStore === "Hiragana" ? 'active-button' : ''}" > <img src="images/hiragana/a.png" alt="hiragana" /> </div>
-        <div on:click={() => {setModule('Katakana')}} class="nav-button img-button {$ModuleStore === "Katakana" ? 'active-button' : ''}" > <img src="images/katakana/a.png" alt="katakana" /> </div>
+        <div on:click={() => {setModule('Hiragana')}} class="nav-button img-button {$ModuleStore === "Kana Chart" || $ModuleStore === "Hiragana" ? 'active-button' : ''}" > <IconKana kana="hiragana" /> </div>
+        <div on:click={() => {setModule('Katakana')}} class="nav-button img-button {$ModuleStore === "Katakana" ? 'active-button' : ''}" ><IconKana kana="katakana" /> </div>
     {/if}
 
     <span on:click={() => {setModule('Home')}} class="home-icon" >
@@ -85,9 +83,4 @@
         z-index: -10;
         opacity: 0.1;
     }
-
-    .img-button > img {
-        width: 100%;
-        height: 100%;
-    }   
 </style>
