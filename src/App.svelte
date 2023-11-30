c8fc7f51b85d8af033d81abb3dd4d20eb7fadfe5
Adi <adi92az@gmail.com>
2023-11-30 11:33:09 +0100

    Add nav component

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 
   import { ModuleStore, WordsStore } from "./stores";
   import { getAllWords } from "./services/supabaseService";
+  import Nav from "./lib/components/Nav.svelte";
 
   import Layout from "./lib/Layout.svelte";
 
@@ -17,11 +18,6 @@
     const data = await getAllWords()
     WordsStore.set(data)
   });
-
-  function goHome() {
-    ModuleStore.set("Home")
-  }
-
 </script>
 
 <main>
@@ -29,15 +25,7 @@
 </main>
 
 {#if $ModuleStore !== "Home"}
-  <div class="nav" >
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <span on:click={goHome} class="home-icon" >
-        <svg width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-        </svg> 
-    </span>
-  </div>
+  <Nav />
 {/if}
 
 <style>
@@ -50,24 +38,5 @@
     display: flex;
     justify-content: center;
     gap: 1rem;
-  }
-
-  .nav {
-    position: sticky;
-    display: flex;
-    justify-content: center;
-    align-content: flex-end;
-    bottom: 0;
-    border-top: 1px solid rgb(205, 205, 205);
-    background-color: #242424;
-  }
-
-  .home-icon {
-    font-size: 1.5625rem;
-  }
-
-  .home-icon:hover {
-    color: red;
-    cursor: pointer;
   }
 </style>
